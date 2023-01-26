2f89cdd7c4a631acff9f9978a8081843d33d1aa1
Willians Vargas <williant.marquez@gmail.com>
2023-01-26 18:24:12 +0100

    [FEAT] Added delete file

--- src/s3/s3.controller.ts
+++ src/s3/s3.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GetFile, LocalFileInterceptor } from '../common';
+import { S3Service } from './s3.service';
 import { UploadFile } from './use-cases/UploadFile';
 
 @Controller()
@@ -8,6 +16,7 @@ export class S3Controller {
   constructor(
     private configService: ConfigService,
     private uploadFileUseCase: UploadFile,
+    private s3Service: S3Service,
   ) {}
 
   @Get('bucket-url')
@@ -17,12 +26,20 @@ export class S3Controller {
     };
   }
 
-  @Post('upload')
+  @Post()
   @UseInterceptors(LocalFileInterceptor())
   uploadFile(
     @Body('folder') folder: string,
     @GetFile() file: Express.Multer.File,
   ) {
     return this.uploadFileUseCase.call(folder, file);
+  }
+
+  @Delete()
+  async deleteFile(@Body('key') key: string) {
+    await this.s3Service.delete(key);
+    return {
+      message: 'File deleted',
+    };
   }
 }
