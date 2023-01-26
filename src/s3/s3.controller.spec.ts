835abd800a88966e782b8e85c78e08ec1dcf96ca
Willians Vargas <williant.marquez@gmail.com>
2023-01-26 19:53:38 +0100

    [TEST] Fixed tests

--- src/s3/s3.controller.spec.ts
+++ src/s3/s3.controller.spec.ts
@@ -1,5 +1,8 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { S3Controller } from './s3.controller';
+import { S3Service } from './s3.service';
+import { UploadFile } from './use-cases/UploadFile';
 
 describe('S3Controller', () => {
   let controller: S3Controller;
@@ -7,6 +10,7 @@ describe('S3Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [S3Controller],
+      providers: [S3Service, ConfigService, UploadFile],
     }).compile();
 
     controller = module.get<S3Controller>(S3Controller);
